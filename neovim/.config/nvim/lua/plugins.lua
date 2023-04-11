0e6383e49368c75f9ec5ab06b5ed5d1e0b088174
kcboschert <kcboschert@gmail.com>
2023-04-10 21:24:15 -0500

    Adding gitsigns to the gutter.

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,1 +1,18 @@
-return { }
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = '+' },
+          change       = { text = '│' },
+          delete       = { text = '-' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        }
+      })
+    end,
+  }
+}
