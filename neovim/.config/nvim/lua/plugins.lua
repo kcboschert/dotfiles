78b63bc97c54c438b624dfbddb6ae597ecc517bd
kcboschert <code@kbos.ch>
2023-04-12 20:37:21 -0500

    Comment toggling plugin & shortcut

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -14,5 +14,15 @@ return {
         }
       })
     end,
-  }
+  },
+
+  {
+    "terrortylor/nvim-comment",
+    keys = {
+      { "<leader>cc", "<cmd>CommentToggle<cr>", desc = "Comment/Uncomment selection", mode = { "n", "v" } },
+    },
+    config = function()
+      require("nvim_comment").setup()
+    end,
+  },
 }
