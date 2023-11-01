31da2c4e4b898bb1355ed153862d541e9b23b5d4
kcboschert <code@kbos.ch>
2023-11-01 00:36:34 -0500

    Upgrading plugins. Indent-blankline setup updated for compatibility.

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -2,18 +2,12 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     keys = {
-      { "<leader>il", "<cmd>IndentBlanklineToggle<cr>", desc = "Toggle Indentlines"}
+      { "<leader>il", "<cmd>IBLToggle<cr>", desc = "Toggle Indentlines"}
     },
+    main = "ibl",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-      vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
-
-      require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-      }
+      require("ibl").setup()
     end,
   },
 }
