a85d39558dd0d971dd567558f54f41de4da4d1ae
kcboschert <code@kbos.ch>
2023-12-07 19:29:03 -0600

    chore: removing custom neovim configuration to prepare for lazyvim

--- neovim/.config/nvim/ftplugin/json.lua
+++
@@ -1,3 +0,0 @@
-local map = require("util").map
-
-map("n", "<leader>F", "<cmd>%!jq .<cr>", { desc = "Apply formatting (buffer)" })
--- neovim/.config/nvim/ftplugin/terraform.lua
+++
@@ -1,4 +0,0 @@
-local map = require("util").map
-
-map("n", "<leader>F", "<cmd>TerraformFmt<cr>", { desc = "Apply formatting (buffer)" })
-vim.api.nvim_create_autocmd({"BufWritePre", "InsertLeave"}, { pattern = { "*.tf" }, command = "TerraformFmt" })
