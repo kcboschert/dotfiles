b6425b181909ed8fe5e852a56af9c5b36fcccbb4
kcboschert <kcboschert@gmail.com>
2023-12-23 02:51:07 -0600

    WIP

--- neovim/nvim/lua/config/options.lua
+++ neovim/nvim/lua/config/options.lua
@@ -3,10 +3,10 @@
 -- Add any additional options here
 local opt = vim.opt
 
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
-opt.expandtab = true
+-- opt.tabstop = 2
+-- opt.shiftwidth = 2
+-- opt.softtabstop = 2
+-- opt.expandtab = true
 
 opt.clipboard = ""
 opt.mouse = ""
--- neovim/nvim/lua/plugins/csharp.lua
+++ neovim/nvim/lua/plugins/csharp.lua
@@ -6,6 +6,9 @@ return {
         timeout_ms = 3000,
       },
       formatters_by_ft = {
+        cs = {},
+      },
+      --[[ formatters_by_ft = {
         cs = { "csharpier" },
       },
       formatters = {
@@ -13,7 +16,7 @@ return {
           command = vim.fn.expand("~/.local/share/nvim/mason/bin/") .. "dotnet-csharpier",
           args = { "--write-stdout" },
         },
-      },
+      }, ]]
       --log_level = vim.log.levels.TRACE,
     },
   },
