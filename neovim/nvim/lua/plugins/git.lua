37337ee76efe742f28dcff3d15f157f9ab7c4d37
kcboschert <kcboschert@gmail.com>
2024-03-22 14:28:28 -0500

    feat: Add github url generating plugin with keymapping.

--- neovim/nvim/lua/plugins/git.lua
+++ neovim/nvim/lua/plugins/git.lua
@@ -6,4 +6,10 @@ return {
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "Blame file" },
     },
   },
+  {
+    "pgr0ss/vim-github-url",
+    keys = {
+      { "<leader>gu", "<cmd>:GitHubURL<cr>", desc = "Generate GitHub Link" },
+    },
+  },
 }
