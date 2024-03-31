8f9adaa7a32db674074dd7977f5aa5f9075b0b1d
kcboschert <kcboschert@gmail.com>
2024-03-30 20:14:31 -0500

    feat: Switch to using asdf for language version management.

--- shell/post-install.sh
+++ shell/post-install.sh
@@ -5,25 +5,48 @@ set -euo pipefail
 # Install/Update zplug
 
 if [ ! -d ${HOME}/.zplug ]; then
-  git clone https://github.com/zplug/zplug.git "${HOME}/.zplug"
+	git clone https://github.com/zplug/zplug.git "${HOME}/.zplug"
 else
-  pushd ${HOME}/.zplug
-  git pull
-  popd
+	pushd ${HOME}/.zplug
+	git pull
+	popd
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  if ! command -v gls >/dev/null 2>&1; then
-    brew install coreutils
-  fi
-  brew tap homebrew/cask-fonts
-  for pkg in \
-    font-fira-code-nerd-font \
-    gnu-sed \
-    gnu-tar \
-    gnu-which \
-    grep \
-  ; do
-    brew install ${pkg}
-  done
+	if ! command -v gls >/dev/null 2>&1; then
+		brew install coreutils
+	fi
+	brew tap homebrew/cask-fonts
+	for pkg in \
+		font-fira-code-nerd-font \
+		gawk \
+		gnu-sed \
+		gnu-tar \
+		gnu-which \
+		gpg \
+		grep; do
+		brew install ${pkg}
+	done
 fi
+
+# Install asdf plugins for common languages
+echo ""
+echo "Installing asdf Version Manager plugins for languages..."
+source ~/.asdf/asdf.sh
+
+declare -A plugins
+plugins[nodejs]=https://github.com/asdf-vm/asdf-nodejs.git
+plugins[ruby]=https://github.com/asdf-vm/asdf-ruby
+plugins[golang]=https://github.com/asdf-community/asdf-golang.git
+
+for lang in "${!plugins[@]}"; do
+	if ! asdf list | grep ${lang} >/dev/null 2>&1; then
+		echo "    ${lang} Installing..."
+		asdf plugin add ${lang} ${plugins[$lang]}
+		asdf install ${lang} latest
+		asdf global ${lang} latest
+		echo "    ...done."
+	else
+		echo "    ${lang} already installed!"
+	fi
+done
