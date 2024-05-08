29b84f1950e20fb4f73c6cc685f4d92e485a1fe7
kcboschert <kcboschert@gmail.com>
2024-05-08 10:58:37 -0500

    fix: Use jdtls binstub for launching with automatic dependency loading.

--- neovim/nvim/lua/plugins/java.lua
+++ neovim/nvim/lua/plugins/java.lua
@@ -1,4 +1,8 @@
+local jdtls_dir = vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/")
+local lombok_path = jdtls_dir .. "lombok.jar"
+
 local opts = {
+  cmd = { jdtls_dir .. "bin/jdtls", "--jvm-arg=-javaagent:" .. lombok_path },
   settings = {
     java = {
       completion = {
@@ -36,26 +40,23 @@ local opts = {
 }
 if os.getenv("JAVA_HOME") ~= nil then
   local java_path = "/Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home/bin/java"
-  local jdtls_dir = vim.fn.expand("$HOME/.local/share/nvim/mason/packages/jdtls/")
-  local lombok_path = jdtls_dir .. "lombok.jar"
   local equinox_launcher_path = vim.fn.globpath(jdtls_dir .. "plugins", "org.eclipse.equinox.launcher_*.jar")
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
   local workspace_dir = vim.fn.expand("$HOME/.workspace/" .. project_name)
 
-  opts["cmd"] = {
+  opts.cmd = {
     java_path,
     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
     "-Dosgi.bundles.defaultStartLevel=4",
     "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dosgi.checkConfiguration=true",
-    "-Dosgi.sharedConfiguration.area=" .. jdtls_dir .. "config_mac",
-    "-Dosgi.sharedConfiguration.area.readOnly=true",
-    "-Dosgi.configuration.cascaded=true",
-    "-Xms1G",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xmx1g",
     "--add-modules=ALL-SYSTEM",
     "--add-opens", "java.base/java.util=ALL-UNNAMED",
     "--add-opens", "java.base/java.lang=ALL-UNNAMED",
     "-javaagent:" .. lombok_path,
+    -- "-Xlog::file=" .. vim.fn.expand("$HOME/.java_jdtls.log"), "-verbose",
 
     "-jar", equinox_launcher_path,
     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
