85a1ab7e2cb08983b2db0362b11e68c7492c10e8
kcboschert <code@kbos.ch>
2024-06-24 09:18:41 -0500

    fix: Ensure atuin config dir exists.

--- shell/stow-custom.sh
+++ shell/stow-custom.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/.config
+mkdir -p $HOME/.config/atuin
 stow --verbose --ignore='atuin/.*' --restow shell
 stow --verbose --dir=$(dirname $0) --target=$HOME/.config/atuin --restow atuin
