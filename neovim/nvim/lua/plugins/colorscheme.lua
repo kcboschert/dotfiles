dc0ddc11654eb14b59015eb0a2cd5ba58e3bae0c
kcboschert <kcboschert@gmail.com>
2024-07-24 23:58:54 -0500

    feat: flow color scheme

--- neovim/nvim/lua/plugins/colorscheme.lua
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -21,6 +21,17 @@ return {
     opts = {},
   },
   {
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true, -- Set transparent background.
+      fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
+      mode = "desaturate", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+      aggressive_spell = false, -- Display colors for spell check.
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight-night",
