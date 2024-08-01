fa7a292603bd427b0ac1bb273fcec3fcd8c1080d
kcboschert <code@kbos.ch>
2024-07-31 23:53:45 -0500

    feat: Additional colorschemes.

--- neovim/nvim/lua/plugins/colorscheme.lua
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
+      transparent = true,
       on_colors = function(colors)
         colors.border = "#0a0b12" -- Darker for clearer separation
       end,
@@ -24,17 +25,36 @@ return {
     "0xstepit/flow.nvim",
     lazy = false,
     priority = 1000,
+    opts = {},
+    config = function()
+      require("flow").setup({
+        transparent = true, -- Set transparent background.
+        fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
+        mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+        aggressive_spell = false, -- Display colors for spell check.
+      })
+    end,
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
     opts = {
-      transparent = true, -- Set transparent background.
-      fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
-      mode = "desaturate", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
-      aggressive_spell = false, -- Display colors for spell check.
+      transparent_bg = true,
+    },
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_bg = true,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "flow",
     },
   },
 }
