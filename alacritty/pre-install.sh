6e87868e45a60075e510e749ea958bcde79fcec7
kcboschert <code@kbos.ch>
2024-11-24 01:30:35 -0600

    chore: Don't install rust/cargo if cargo is already installed. Move to a PATH that .desktop recognizes.

--- alacritty/pre-install.sh
+++ alacritty/pre-install.sh
@@ -13,16 +13,23 @@ elif [[ "$(uname)" == "Linux" ]]; then
 		# https://github.com/alacritty/alacritty/blob/master/INSTALL.md
 		sudo apt-get install cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 		# need cargo for installing
-		curl https://sh.rustup.rs -sSf | sh
+		if ! command -v cargo >/dev/null 2>&1; then
+			curl https://sh.rustup.rs -sSf | sh
+		fi
 
 		cargo install alacritty
+		if [[ ! -f /usr/local/bin/alacritty ]]; then
+			ln -s "$(which alacritty)" /usr/local/bin/alacritty
+		fi
 
 		# setup desktop entry
 		git clone git@github.com:alacritty/alacritty.git alacritty-install
+
 		pushd alacritty-install
 		sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 		sudo desktop-file-install extra/linux/Alacritty.desktop
 		sudo update-desktop-database
+
 		popd
 		rm -rf alacritty-install
 	fi
