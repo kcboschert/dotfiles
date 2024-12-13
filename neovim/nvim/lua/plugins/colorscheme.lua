4a7753b2ade24584f4d8e2df40685784eac649c1
kcboschert <kcboschert@gmail.com>
2024-12-12 23:52:15 -0600

    fix: Tweak everforest background color.

--- neovim/nvim/lua/plugins/colorscheme.lua
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -63,7 +63,7 @@ return {
     priority = 1000,
     config = function()
       require("everforest").setup({
-        background = "medium",
+        background = "hard",
         transparent_background_level = 1,
         italics = true,
         disable_italic_comments = false,
