d26939b8655f1c7e6bfc7b14cc9bfec63691f5d1
kcboschert <kcboschert@gmail.com>
2024-12-12 23:52:45 -0600

    feat: Re-enable csharpier.

--- neovim/nvim/lua/plugins/csharp.lua
+++ neovim/nvim/lua/plugins/csharp.lua
@@ -5,10 +5,8 @@ return {
       format = {
         timeout_ms = 3000,
       },
+      -- formatters_by_ft = { cs = {} },
       formatters_by_ft = {
-        cs = {},
-      },
-      --[[ formatters_by_ft = {
         cs = { "csharpier" },
       },
       formatters = {
@@ -16,8 +14,8 @@ return {
           command = vim.fn.expand("~/.local/share/nvim/mason/bin/") .. "dotnet-csharpier",
           args = { "--write-stdout" },
         },
-      }, ]]
-      --log_level = vim.log.levels.TRACE,
+      },
+      log_level = vim.log.levels.TRACE,
     },
   },
 }
