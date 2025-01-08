fe572b1154c34d0a8743835b5e29b51bf57a53af
kcboschert <kcboschert@gmail.com>
2025-01-07 19:54:39 -0600

    chore: Remove Luasnip.

--- neovim/nvim/lua/config/lazy.lua
+++ neovim/nvim/lua/config/lazy.lua
@@ -11,7 +11,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
