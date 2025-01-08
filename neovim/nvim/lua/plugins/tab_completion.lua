ac3b603c8bb027b3d8e67f4b732f32c46dddd764
kcboschert <code@kbos.ch>
2025-01-07 19:36:31 -0600

    fix: Use hash of options for blink.cmp. Add type hints for the hash for diagnostics.

--- neovim/nvim/lua/plugins/tab_completion.lua
+++ neovim/nvim/lua/plugins/tab_completion.lua
@@ -1,11 +1,14 @@
+-- https://cmp.saghen.dev/configuration/general.html
 return {
   "saghen/blink.cmp",
-  opts = function(_, opts)
-    opts.completion = { list = { selection = "auto_insert" } }
-    opts.keymap = {
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    completion = { list = { selection = { preselect = false, auto_insert = true } } },
+    keymap = {
       preset = "enter",
       ["<Tab>"] = { "select_next", "fallback" },
       ["<S-Tab>"] = { "select_prev", "fallback" },
-    }
-  end,
+    },
+  },
 }
