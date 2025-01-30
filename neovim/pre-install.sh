e95677dabae6ce219a4e137bfe46dbbc5ad56413
kcboschert <code@kbos.ch>
2025-01-30 11:30:26 -0600

    fix: Neovim install process

--- neovim/pre-install.sh
+++ neovim/pre-install.sh
@@ -5,10 +5,10 @@ set -o nounset -o pipefail -o errexit
 function install_nvim_linux() {
 	sudo apt-get install gcc g++
 
-	curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+	curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 	tar xzvf nvim.tar.gz
-	sudo cp -r nvim-linux64/* /usr/local
-	rm -rf nvim-linux64 nvim.tar.gz
+	sudo cp -r nvim-linux-x86_64/* /usr/local
+	rm -rf nvim-linux-x86_64 nvim.tar.gz
 }
 
 if [[ "$(uname)" == "Linux" ]] && ! command -v brew >/dev/null 2>&1; then
