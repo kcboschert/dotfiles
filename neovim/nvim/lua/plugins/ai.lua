f32ba4dc73bff00c08da938ba20d30f03b25a598
kcboschert <code@kbos.ch>
2025-05-07 01:13:19 -0500

    feat: Disable automatic FIM and remap accept suggestion keybinds.

--- neovim/nvim/lua/plugins/ai.lua
+++ neovim/nvim/lua/plugins/ai.lua
@@ -122,10 +122,13 @@ return {
   },
   {
     "ggml-org/llama.vim",
-    -- init = function()
-    --   vim.g.llama_config = {
-    --     keymap_accept_full = "<C-s>",
-    --   }
-    -- end
+    init = function()
+      vim.g.llama_config = {
+        auto_fim = false,
+        keymap_accept_line = "<C-s>",
+        keymap_accept_full = "<C-b>",
+        keymap_accept_word = nil,
+      }
+    end,
   },
 }
