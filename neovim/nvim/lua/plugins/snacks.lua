7eb9a2df85a41de890c6380c4f742d8a8e136b78
kcboschert <code@kbos.ch>
2025-05-10 00:42:27 -0500

    feat: show ignored and hidden files by default in snacks picker.

---
+++ neovim/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      sources = {
+        files = { hidden = true, ignored = true },
+        explorer = { hidden = true, ignored = true },
+        grep = { hidden = true, ignored = true },
+      },
+    },
+  },
+}
