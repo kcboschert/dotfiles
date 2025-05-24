06cf0d5a87e9cbddd3e608b0463530eb649ff22d
kcboschert <kcboschert@gmail.com>
2025-05-23 20:00:07 -0500

    feat: Podman

---
+++ podman/pre-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+set -o nounset -o pipefail -o errexit
+
+brew install podman \
+	docker-compose
+
+# if we're on linux, install the uidmap package
+if [[ "$(uname)" == "Linux" ]]; then
+	sudo apt-get install uidmap
+fi
