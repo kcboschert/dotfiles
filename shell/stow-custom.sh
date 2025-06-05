14fba9fd7fd7b54f4e9b4e86838636d290f51bd2
kcboschert <code@kbos.ch>
2025-06-04 20:46:58 -0500

    feat: Bottom system monitor.

--- shell/stow-custom.sh
+++ shell/stow-custom.sh
@@ -5,4 +5,5 @@ set -o nounset -o pipefail -o errexit
 mkdir -p $HOME/.config/atuin
 stow --verbose --target="${HOME}" --ignore='atuin.*' --ignore='starship.*' --restow shell
 stow --verbose --dir=$(dirname $0) --target=${HOME}/.config/atuin --restow atuin
+stow --verbose --dir=$(dirname $0) --target=${HOME}/.config/bottom --restow bottom
 stow --verbose --dir=$(dirname $0) --target=${HOME}/.config --no-folding --restow starship
