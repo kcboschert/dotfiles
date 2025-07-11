c197c7ee02aafcd7b5e4446c775ea6380b492595
kcboschert <code@kbos.ch>
2025-07-11 15:00:01 -0500

    feat(neovim): Set expandtab=true to automatically convert leading tabs to spaces.

--- neovim/nvim/lua/config/options.lua
+++ neovim/nvim/lua/config/options.lua
@@ -6,7 +6,7 @@ local opt = vim.opt
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
-opt.expandtab = false
+opt.expandtab = true
 
 opt.completeopt = "menuone,noselect,popup"
 opt.autoindent = true
