b6bd18387f8acb5bf9fac66d09ae72a39bfa8e9a
kcboschert <code@kbos.ch>
2025-07-31 23:05:39 -0500

    feat(llamavim): Update keybindings for accepting line vs full completion.

--- neovim/nvim/lua/plugins/ai.lua
+++ neovim/nvim/lua/plugins/ai.lua
@@ -4,8 +4,8 @@ return {
     init = function()
       vim.g.llama_config = {
         auto_fim = false,
-        keymap_accept_line = "<C-s>",
-        keymap_accept_full = "<C-b>",
+        keymap_accept_line = "<C-n>",
+        keymap_accept_full = "<C-s>",
         keymap_accept_word = nil,
       }
     end,
