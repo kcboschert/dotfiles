dd6900f8b22c67f6113bdce7715e1e4b94a43f18
kcboschert <code@kbos.ch>
2025-08-15 20:27:48 -0500

    feat: Wezterm configuration.

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 10
+config.color_scheme = "Atom"
+-- config.color_scheme = "catppuccin-mocha"
+-- config.color_scheme = "Dracula"
+-- config.color_scheme = "GruvboxDarkHard"
+-- config.color_scheme = "Gooey (Gogh)"
+-- config.color_scheme = "tokyonight_night"
+-- config.color_scheme = "Tomorrow Night Burns"
+
+return config
