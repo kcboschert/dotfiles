ca74fdfed296e16b103f586dd7310d51fd5b9df5
kcboschert <code@kbos.ch>
2025-09-08 22:43:33 -0500

    feat: tokyonight-night colorscheme.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,12 +5,15 @@ local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 10
-config.color_scheme = "Atom"
+-- config.color_scheme = "Atom"
 -- config.color_scheme = "catppuccin-mocha"
 -- config.color_scheme = "Dracula"
 -- config.color_scheme = "GruvboxDarkHard"
 -- config.color_scheme = "Gooey (Gogh)"
--- config.color_scheme = "tokyonight_night"
+-- config.color_scheme = "nord"
+config.color_scheme = "tokyonight_night"
 -- config.color_scheme = "Tomorrow Night Burns"
+-- config.color_scheme = "UltraViolent"
+-- config.color_scheme = "Unikitty Dark (base16)"
 
 return config
