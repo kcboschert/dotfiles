04c25464dd6c9ee2eff6adf2481cae1f6f6dde21
kcboschert <code@kbos.ch>
2025-09-20 11:32:40 -0500

    feat(nvim): Upgrade LazyVim to 15.2.0. Requires tree-sitter-cli, which we now install via Mason.

--- neovim/pre-install.sh
+++ neovim/pre-install.sh
@@ -3,17 +3,25 @@
 set -o nounset -o pipefail -o errexit
 
 function install_nvim_linux() {
-	sudo apt-get install gcc g++
+  sudo apt-get install gcc g++
 
-	curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
-	tar xzvf nvim.tar.gz
-	sudo cp -r nvim-linux-x86_64/* /usr/local
-	rm -rf nvim-linux-x86_64 nvim.tar.gz
+  curl -L -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
+  tar xzvf nvim.tar.gz
+  sudo cp -r nvim-linux-x86_64/* /usr/local
+  rm -rf nvim-linux-x86_64 nvim.tar.gz
 }
 
 if [[ "$(uname)" == "Linux" ]] && ! command -v brew >/dev/null 2>&1; then
-	install_nvim_linux
-	sudo apt-get install bison ripgrep fzf luarocks
+  install_nvim_linux
+  sudo apt-get install bison ripgrep fzf luarocks
 else
-	brew install neovim bison ripgrep pybind11 fd lazygit fzf luarocks
+  brew install \
+    neovim \
+    bison \
+    ripgrep \
+    pybind11 \
+    fd \
+    lazygit \
+    fzf \
+    luarocks
 fi
