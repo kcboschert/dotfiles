7d9cfe3cea5c8c42463291989d38ee53bd2fb10a
kcboschert <kcboschert@gmail.com>
2025-10-06 09:39:03 -0500

    feat(wezterm): Disable auditory bell and enable visual bell.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,4 +16,18 @@ config.color_scheme = "tokyonight_night"
 -- config.color_scheme = "UltraViolent"
 -- config.color_scheme = "Unikitty Dark (base16)"
 
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+config.colors = {
+  visual_bell = '#160813'
+}
+-- bg = "#1a1b26",
+--   bg_dark = "#16161e",
+--   bg_dark1 = "#0C0E14",
+config.audible_bell = "Disabled"
+
 return config
