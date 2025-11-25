b5cc0c11ed7bfb236f66463ee4799bd6a1a75b10
kcboschert <code@kbos.ch>
2025-11-24 19:34:00 -0600

    feat: Install Bamboo colorscheme in neovim and add as an option to Wezterm config. Add background transparency to Wezterm.

--- neovim/nvim/lua/plugins/colorscheme.lua
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -74,9 +74,20 @@ return {
     end,
   },
   {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup({
+        -- optional configuration here
+      })
+      -- require("bamboo").load()
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "tokyonight",
     },
   },
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,29 +1,36 @@
+-- https://wezterm.org/config/files.html
+
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 0.91
+
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 10
 -- config.color_scheme = "Atom"
+-- config.color_scheme = "Bamboo"
+-- config.color_scheme = "carbonfox"
 -- config.color_scheme = "catppuccin-mocha"
 -- config.color_scheme = "Dracula"
 -- config.color_scheme = "GruvboxDarkHard"
 -- config.color_scheme = "Gooey (Gogh)"
 -- config.color_scheme = "nord"
-config.color_scheme = "tokyonight_night"
+config.color_scheme = "Tokyo Night"
 -- config.color_scheme = "Tomorrow Night Burns"
 -- config.color_scheme = "UltraViolent"
 -- config.color_scheme = "Unikitty Dark (base16)"
 
 config.visual_bell = {
-  fade_in_function = 'EaseIn',
-  fade_in_duration_ms = 150,
-  fade_out_function = 'EaseOut',
-  fade_out_duration_ms = 150,
+	fade_in_function = "EaseIn",
+	fade_in_duration_ms = 150,
+	fade_out_function = "EaseOut",
+	fade_out_duration_ms = 150,
 }
 config.colors = {
-  visual_bell = '#160813'
+	visual_bell = "#160813",
 }
 -- bg = "#1a1b26",
 --   bg_dark = "#16161e",
