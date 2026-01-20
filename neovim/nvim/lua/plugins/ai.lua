48764ceff2921e654c7b78477229ca9d52ac0738
kcboschert <code@kbos.ch>
2026-01-20 14:42:30 -0600

    fix: Update llama.vim config to use updated/non-deprecated option name.

--- neovim/nvim/lua/plugins/ai.lua
+++ neovim/nvim/lua/plugins/ai.lua
@@ -4,9 +4,9 @@ return {
     init = function()
       vim.g.llama_config = {
         auto_fim = false,
-        keymap_accept_line = "<C-n>",
-        keymap_accept_full = "<C-s>",
-        keymap_accept_word = nil,
+        keymap_fim_accept_line = "<C-n>",
+        keymap_fim_accept_full = "<C-s>",
+        keymap_fim_accept_word = nil,
       }
     end,
   },
