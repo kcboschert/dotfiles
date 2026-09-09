610dcbc32e26dc81865bd76343137d3793b3719a
kcboschert <code@kbos.ch>
2026-09-08 19:52:20 -0500

    refactor: Use mise bootstrap for installing shell module.

--- shell/stow-custom.sh
+++ shell/stow-custom.sh
@@ -2,7 +2,4 @@
 
 set -o nounset -o pipefail -o errexit
 
-mkdir -p $HOME/.config/atuin
-stow --verbose --target="${HOME}" --ignore='atuin.*' --ignore='starship.*' --restow shell
-stow --verbose --dir=$(dirname $0) --target=${HOME}/.config/atuin --restow atuin
-stow --verbose --dir=$(dirname $0) --target=${HOME}/.config --no-folding --restow starship
+mise bootstrap
