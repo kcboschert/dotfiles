d109e9b21d86322c38c971ec3b35ca531a71546a
kcboschert <code@kbos.ch>
2026-09-08 19:52:19 -0500

    refactor: Use mise bootstrap for installing tmux.

--- tmux/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset -o pipefail -o errexit
-
-${HOME}/.tmux/plugins/tpm/scripts/clean_plugins.sh
-${HOME}/.tmux/plugins/tpm/scripts/install_plugins.sh
-${HOME}/.tmux/plugins/tpm/scripts/update_plugin.sh
